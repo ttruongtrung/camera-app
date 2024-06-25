05897f9ab264e81d811033ebb6bb41bc2f6956e8
Thanh Truong <thanhtt.uit@gmail.com>
2024-06-25 22:39:40 +0700

    keep login session

--- frontend/src/auth/AuthContext.js
+++ frontend/src/auth/AuthContext.js
@@ -1,16 +1,34 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext();
+
 const AuthProvider = ({ children }) => {
-  const [accessToken, setAccessToken] = useState(null);
+  const [accessToken, setAccessToken] = useState(() => {
+    const token = localStorage.getItem('accessToken');
+    return token ? token : null;
+  });
 
   const login = (token) => {
     setAccessToken(token);
+    localStorage.setItem('accessToken', token);
   };
 
   const logout = () => {
     setAccessToken(null);
+    localStorage.removeItem('accessToken');
   };
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      const token = localStorage.getItem('accessToken');
+      setAccessToken(token ? token : null);
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, []);
 
   return (
     <AuthContext.Provider value={{ accessToken, login, logout }}>
