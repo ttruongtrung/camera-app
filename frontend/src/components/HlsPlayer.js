fe8188fe2b15fe8939ea8fa55b1d1aca7c76e7bd
Thanh Truong <thanhtt.uit@gmail.com>
2024-06-27 22:44:42 +0700

    hot fix 2

--- frontend/src/components/HlsPlayer.js
+++ frontend/src/components/HlsPlayer.js
@@ -1,38 +1,48 @@
 import React, { useEffect, useRef } from 'react';
 import Hls from 'hls.js';
+
 const apiPath = process.env.REACT_APP_BE_API_URL;
 
 const HLSPlayer = ({ cameraId }) => {
   const videoRef = useRef(null);
   const hlsRef = useRef(null);
-  const videoSrc = `${apiPath}/hls/stream_${cameraId}.m3u8`;
 
   useEffect(() => {
     const video = videoRef.current;
-    let hls;
+    const videoSrc = `${apiPath}/hls/stream_${cameraId}.m3u8`;
 
-    if (Hls.isSupported()) {
-      const hls = new Hls();
-      hls.loadSource(videoSrc);
-      hls.attachMedia(video);
-      hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        video.play();
-      });
-      hlsRef.current = hls;
-    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-      video.src = videoSrc;
-      video.addEventListener('loadedmetadata', () => {
-        video.play();
-      });
-    }
+    const initializeHLS = () => {
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(videoSrc);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          video.play().catch(error => {
+            console.error('Error playing video:', error);
+          });
+        });
+        hlsRef.current = hls;
+      } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = videoSrc;
+        video.addEventListener('loadedmetadata', () => {
+          video.play().catch(error => {
+            console.error('Error playing video:', error);
+          });
+        });
+      }
+    };
 
+    // Initialize HLS.js when component mounts or cameraId changes
+    initializeHLS();
+
+    // Cleanup function
     return () => {
-      // if (Hls.isSupported()) {
-      //   hls.destroy();
-      // }
       if (hlsRef.current) {
         hlsRef.current.destroy();
         hlsRef.current = null;
+      } else {
+        video.pause();
+        video.src = ''; // Clear source to stop playback
       }
     };
   }, [cameraId]);
@@ -41,3 +51,5 @@ const HLSPlayer = ({ cameraId }) => {
 };
 
 export default HLSPlayer;
+
+
